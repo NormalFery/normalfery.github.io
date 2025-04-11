03979efe53a351af87abc9d714af70c402020d7f
NormalFery <132052650+NormalFery@users.noreply.github.com>
2025-04-11 14:00:31 +0100

    feat: Added and implemented Theme Changer

---
+++ javascript/themes.js
@@ -0,0 +1,16 @@
+function changeStyle(sheet) {
+    document.getElementById("css").setAttribute("href", sheet);
+}
+
+function dreamy() {
+    changeStyle("../styles/dreamy.css")
+
+}
+function green() {
+    changeStyle("../styles/turquoise.css")
+
+}
+function cotton() {
+     changeStyle("../styles/cotton-candy.css")
+
+}
