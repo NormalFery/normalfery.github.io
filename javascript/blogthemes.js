f5279cae82746f33ff4a40933b42c13298c73eef
NormalFery <fernandolugoalonso@gmail.com>
2025-06-11 09:54:40 +0100

    fix: Fixed theme errors caused by refactorization

---
+++ javascript/blogthemes.js
@@ -0,0 +1,93 @@
+function changeStyle(sheet) {
+    document.getElementById("css").setAttribute("href", sheet);
+}
+
+function dreamy() {
+    changeStyle("../../styles/dreamy.css")
+    setCookie('theme', 'dreamy', 365)
+
+}
+function green() {
+    changeStyle("../../styles/turquoise.css")
+    setCookie('theme', 'turquoise', 365)
+
+}
+function cotton() {
+     changeStyle("../../styles/cotton-candy.css")
+     setCookie('theme', 'cotton-candy', 365)
+
+}
+
+
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function deleteCookie() {
+    setCookie('theme', 'dreamy', -1);
+    location.reload();
+}
+
+function dark() {
+    changeStyle("../../styles/monochrome.css")
+    setCookie('theme', 'dark', 365)
+}
+
+function changelog() {
+    changeStyle("../../styles/changelog.css")
+    setCookie('theme', 'changelog', 365)
+}
+
+function checkCookie() {
+    // We use a switch statement because I REALLY don't feel like doing magic, and they're in here for SOMETHING
+    let theme = getCookie("theme");
+    if (theme !== "") {
+        switch (theme) {
+            case "turquoise":
+                green()
+                break;
+            case "cotton-candy":
+                cotton()
+                break;
+            case "dreamy":
+                dreamy()
+                break;
+            case "dark":
+                dark()
+                break;
+            case "changelog":
+                changelog()
+                break;
+            case "delete":
+                deleteCookie()
+                break;
+            case "default":
+                // We set the default case (Not a valid cookie) to delete it IMMEDIATELY - I don't feel like dealing with cookies too much
+                document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                break;
+        }
+    } else {
+        // console.log("Temporal code - can an else be empty?")
+        // THe answer is, yes :3
+    }
+}
+
+checkCookie();
