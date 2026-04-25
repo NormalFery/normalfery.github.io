1ab73cfc1f3cbfdf4ea264b219a2041e4fb365ef
Fery <me@normalfery.es>
2026-04-25 11:30:56 +0100

    feat: Implement memory API for memory listing

--- assets/javascript/api-blog.js
+++ assets/javascript/api-blog.js
@@ -1,27 +1,49 @@
 document.addEventListener("DOMContentLoaded", loaded);
 
 async function loaded() {
-    const url = "https://api.normalfery.es/listBlog";
+    const urlBlog = "https://api.normalfery.es/listBlog";
     const blogDiv = document.getElementsByClassName("bodies")[0];
+    const urlMemory = "https://api.normalfery.es/listMemory";
+    const memoryDiv = document.getElementsByClassName("bodies")[1];
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`I FUCKED UP OH GOD: ${response.status}`);
-        }
-
-        const result = await response.json();
-        result.entries.forEach(function(blog){
-            const blogHtml =
-                "<p>" +
-                "<a href='entry/" + blog.filename + "'>" + blog.title + "</a> <br>" +
-                blog.description +
-                "</p>";
-            blogDiv.innerHTML += blogHtml;
-        })
-
-
-
+        await getBlog(urlBlog, blogDiv);
+        await getMemory(urlMemory, memoryDiv);
     } catch (error) {
         console.error(error.message);
     }
+}
+
+async function getBlog(urlBlog, blogDiv) {
+    const response = await fetch(urlBlog);
+    if (!response.ok) {
+        throw new Error(`I FUCKED UP OH GOD: ${response.status}`);
+    }
+
+    const result = await response.json();
+    result.entries.forEach(function(blog){
+        const blogHtml =
+            "<p>" +
+            "<a href='entry/" + blog.filename + "'>" + blog.title + "</a> <br>" +
+            blog.description +
+            "</p>";
+        blogDiv.innerHTML += blogHtml;
+    })
+}
+
+async function getMemory(urlMemory, memoryDiv) {
+    const response = await fetch(urlMemory);
+    if (!response.ok) {
+        throw new Error(`FUCK. (Memory): ${response.status}`);
+
+    }
+
+    const result = await response.json();
+    result.entries.forEach(function(blog){
+        const memoryHtml =
+            "<p>" +
+            "<a href='entry/" + blog.filename + "'>" + blog.title + "</a> <br>" +
+            blog.description +
+            "</p>";
+        memoryDiv.innerHTML += memoryHtml;
+    })
 }
